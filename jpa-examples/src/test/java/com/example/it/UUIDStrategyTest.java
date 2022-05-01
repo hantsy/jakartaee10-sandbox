fa7aa018ef0838c982d80c8613078fa80c1d2541
hantsy <hantsy@gmail.com>
2022-05-01 14:10:17 +0800

    chore: clean codes

---
+++ jpa-examples/src/test/java/com/example/it/UUIDStrategyTest.java
@@ -0,0 +1,95 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+ */
+package com.example.it;
+
+import com.example.Person;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(ArquillianExtension.class)
+public class UUIDStrategyTest {
+
+    private final static Logger LOGGER = Logger.getLogger(UUIDStrategyTest.class.getName());
+
+    @Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class)
+                .addClass(Person.class)
+                .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    UserTransaction ux;
+
+    @BeforeEach
+    public void before() throws Exception {
+        startTx();
+    }
+
+    private void startTx() throws Exception {
+        ux.begin();
+        em.joinTransaction();
+    }
+
+    @AfterEach
+    public void after() throws Exception {
+        endTx();
+    }
+
+    private void endTx() throws Exception {
+        try {
+            ux.commit();
+        } catch (Exception e) {
+            ux.rollback();
+        }
+    }
+
+    @Test
+    public void testPersistingPersons() throws Exception {
+        final Person person = new Person();
+        person.setName("Hantsy Bai");
+        em.persist(person);
+        endTx();
+
+        startTx();
+        final Person foundPerson = em.find(Person.class, person.getId());
+        assertNotNull(foundPerson.getId());
+        LOGGER.log(Level.INFO, "Found person: {0}", foundPerson);
+    }
+}
