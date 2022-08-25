34783763939029956fcf39d01298cc1244796d8b
hantsy <hantsy@gmail.com>
2022-08-25 17:18:03 +0800

    chore: bump to Glassfish 7.0.0-M8

--- security-oidc/src/main/java/com/example/ProtectedServlet.java
+++ security-oidc/src/main/java/com/example/ProtectedServlet.java
@@ -1,10 +1,8 @@
 package com.example;
 
 import jakarta.annotation.security.DeclareRoles;
-import jakarta.inject.Inject;
 import jakarta.security.enterprise.authentication.mechanism.http.OpenIdAuthenticationMechanismDefinition;
 import jakarta.security.enterprise.authentication.mechanism.http.openid.OpenIdProviderMetadata;
-import jakarta.security.enterprise.identitystore.openid.OpenIdContext;
 import jakarta.servlet.annotation.HttpConstraint;
 import jakarta.servlet.annotation.ServletSecurity;
 import jakarta.servlet.annotation.WebServlet;
@@ -29,19 +27,19 @@ import java.io.IOException;
 @ServletSecurity(@HttpConstraint(rolesAllowed = "foo"))
 public class ProtectedServlet extends HttpServlet {
 
-    @Inject
-    private OpenIdContext context;
+//    @Inject
+//    private OpenIdContext context;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Secured Servlet</h1>");
-        response.getWriter().println("<p>access token:" + context.getAccessToken() + "</p>");
-        response.getWriter().println("<p>token type:" + context.getTokenType() + "</p>");
-        response.getWriter().println("<p>subject:" + context.getSubject() + "</p>");
-        response.getWriter().println("<p>expires in:" + context.getExpiresIn() + "</p>");
-        response.getWriter().println("<p>refresh token:" + context.getRefreshToken() + "</p>");
-        response.getWriter().println("<p>claims json:" + context.getClaimsJson() + "</p>");
+        response.getWriter().println("<h1>Protected Servlet</h1>");
+//        response.getWriter().println("<p>access token:" + context.getAccessToken() + "</p>");
+//        response.getWriter().println("<p>token type:" + context.getTokenType() + "</p>");
+//        response.getWriter().println("<p>subject:" + context.getSubject() + "</p>");
+//        response.getWriter().println("<p>expires in:" + context.getExpiresIn() + "</p>");
+//        response.getWriter().println("<p>refresh token:" + context.getRefreshToken() + "</p>");
+//        response.getWriter().println("<p>claims json:" + context.getClaimsJson() + "</p>");
     }
 }
 
