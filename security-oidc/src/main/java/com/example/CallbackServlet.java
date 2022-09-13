a8ad06d9e293d0194f6c893e998f13f25f2b4a70
hantsy <hantsy@gmail.com>
2022-09-13 23:56:24 +0800

    chore: clean codes

--- security-oidc/src/main/java/com/example/CallbackServlet.java
+++ security-oidc/src/main/java/com/example/CallbackServlet.java
@@ -1,25 +1,30 @@
 package com.example;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-
 @WebServlet("/callback")
 public class CallbackServlet extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(CallbackServlet.class.getName());
 
-//    @Inject
-//    private OpenIdContext context;
+    // @Inject
+    // private OpenIdContext context;
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //response.getWriter().println(context.getAccessToken());
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // response.getWriter().println(context.getAccessToken());
 
         String referer = (String) request.getSession().getAttribute("Referer");
-        String redirectTo = referer != null ? referer : "/protected";
+        String redirectTo = referer != null ? referer : "/";
+        LOGGER.log(Level.INFO, "redirect to: {0}", redirectTo);
 
         response.sendRedirect(redirectTo);
     }
