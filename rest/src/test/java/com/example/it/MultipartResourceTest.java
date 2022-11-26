a14c21bf126c64ff9f1f43520e82a3378fd672be
hantsy <hantsy@gmail.com>
2022-11-27 00:09:38 +0800

    chore: clean codes

--- rest/src/test/java/com/example/it/MultipartResourceTest.java
+++ rest/src/test/java/com/example/it/MultipartResourceTest.java
@@ -116,8 +116,7 @@ public class MultipartResourceTest {
                         .mediaType(MediaType.APPLICATION_SVG_XML_TYPE)
                         .build()
         );
-        var genericEntity = new GenericEntity<List<EntityPart>>(parts) {
-        };
+        var genericEntity = new GenericEntity<List<EntityPart>>(parts) {};
         var entity = Entity.entity(genericEntity, MediaType.MULTIPART_FORM_DATA);
         Response r = target.request().post(entity);
         assertEquals(200, r.getStatus());
@@ -132,8 +131,7 @@ public class MultipartResourceTest {
 
         assertEquals(200, response.getStatus());
         LOGGER.log(Level.INFO, "GetFiles response status: {0}", response.getStatus());
-        List<EntityPart> parts = response.readEntity(new GenericType<List<EntityPart>>() {
-        });
+        List<EntityPart> parts = response.readEntity(new GenericType<List<EntityPart>>() {});
         parts.forEach(part -> LOGGER.log(
                 Level.INFO,
                 "Get file: {0},{1},{2},{3}",
